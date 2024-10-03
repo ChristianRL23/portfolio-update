16527797cea055a5f1305fd0d14b521b7dd43bb5
Christian RL <87323279+ChristianRL23@users.noreply.github.com>
2024-10-03 10:56:06 -0600

    Update.

--- src/layout/Hero/Hero.jsx
+++ src/layout/Hero/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
     <header className="hero">
       <div className="hero__title">
         <h1 className="hero__title-name">Christian Uriel Ramírez Lara</h1>
-        <h2 className="hero__title-role">JavaScript & Java Developer</h2>
+        <h2 className="hero__title-role">JavaScript & TypeScript Developer</h2>
         <Button onClick={openPDF} textContent="Download CV" />
       </div>
     </header>
